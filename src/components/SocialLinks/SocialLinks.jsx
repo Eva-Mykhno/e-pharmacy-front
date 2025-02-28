aaac6836d1fdb73d9b3a9742329d2969f0521ecf
Eva-Mykhno <154619097+Eva-Mykhno@users.noreply.github.com>
2025-02-28 12:51:07 +0200

    add social links

--- src/components/SocialLinks/SocialLinks.jsx
+++ src/components/SocialLinks/SocialLinks.jsx
@@ -1,5 +1,48 @@
+import s from "./SocialLinks.module.css";
+
+const sprite = "/sprite.svg";
+
 const SocialLinks = () => {
-  return <div>SocialLinks</div>;
+  return (
+    <ul className={s.list}>
+      <li className={s.item}>
+        <a
+          href="https://www.facebook.com/goITclub/"
+          target="_blank"
+          rel="noopener noreferrer">
+          <div className={s.wrap}>
+            <svg className={s.icon}>
+              <use href={`${sprite}#icon-facebook`} />
+            </svg>
+          </div>
+        </a>
+      </li>
+      <li className={s.item}>
+        <a
+          href="https://www.instagram.com/goitclub/"
+          target="_blank"
+          rel="noopener noreferrer">
+          <div className={s.wrap}>
+            <svg className={s.icon}>
+              <use href={`${sprite}#icon-instagram`} />
+            </svg>
+          </div>
+        </a>
+      </li>
+      <li className={s.item}>
+        <a
+          href="https://www.youtube.com/c/GoIT"
+          target="_blank"
+          rel="noopener noreferrer">
+          <div className={s.wrap}>
+            <svg className={s.icon}>
+              <use href={`${sprite}#icon-youtube`} />
+            </svg>
+          </div>
+        </a>
+      </li>
+    </ul>
+  );
 };
 
 export default SocialLinks;
